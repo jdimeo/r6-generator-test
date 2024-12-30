dded7983ae64d50a617d894af60be71877371d47
John Dimeo <dimeo@elderresearch.com>
2024-12-30 14:21:00 -0500

    More complete example that actually activates the plugin

---
+++ java/src/main/java/io/github/jdimeo/R6HelloWorld.java
@@ -0,0 +1,12 @@
+package io.github.jdimeo;
+
+import uk.co.terminological.rjava.RClass;
+import uk.co.terminological.rjava.RMethod;
+
+@RClass
+public class R6HelloWorld {
+	@RMethod(examples = "rgentest::helloWorld()")
+	public void helloWorld() {
+		System.out.println("Hello, world!");
+	}
+}
